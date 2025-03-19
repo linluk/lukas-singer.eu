2b974c26d75e7fb249caa4088b8409d07e24764f
Lukas Singer <lukas42singer@gmail.com>
2025-03-19 10:28:57 +0100

    scp to publish, ssh to connect, credentials in ignored secret.sh

---
+++ scp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -f secrets.sh ]]; then
+    . secrets.sh
+    if [[ -z "${SECRET_SCP_IDENTITY_FILE:-}" ]]; then
+        echo "SECRET_SCP_IDENTITY_FILE is not set or empty. Check secrets.sh" 1>&2
+        exit 1
+    fi
+    if [[ -z "${SECRET_SCP_USER:-}" ]]; then
+        echo "SECRET_SCP_USER is not set or empty. Check secrets.sh" 1>&2
+        exit 1
+    fi
+    scp -i "$SECRET_SCP_IDENTITY_FILE" -r www/* scp://$SECRET_SCP_USER@lukas-singer.eu/public_html/
+else
+    echo "secret.sh not found." 1>&2
+    exit 1
+fi
---
+++ ssh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -f secrets.sh ]]; then
+    . secrets.sh
+    if [[ -z "${SECRET_SSH_IDENTITY_FILE:-}" ]]; then
+        echo "SECRET_SSH_IDENTITY_FILE is not set or empty. Check secrets.sh" 1>&2
+        exit 1
+    fi
+    if [[ -z "${SECRET_SSH_USER:-}" ]]; then
+        echo "SECRET_SSH_USER is not set or empty. Check secrets.sh" 1>&2
+        exit 1
+    fi
+    ssh -i "$SECRET_SSH_IDENTITY_FILE" $SECRET_SSH_USER@lukas-singer.eu
+else
+    echo "secret.sh not found." 1>&2
+    exit 1
+fi
