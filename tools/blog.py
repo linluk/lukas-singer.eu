7429fc9b50878a4c50c5ac1a0e68057cc4fcb822
Lukas Singer <lukas42singer@gmail.com>
2025-03-22 09:51:08 +0100

    restructured "template" handling. added some rss.

--- tools/blog.py
+++ tools/blog.py
@@ -10,8 +10,10 @@ Arguments:
 -h, --help                print this help text and exits without error.
 
 -u, --update              reads the yaml headers from all markdown files under
-                          './src/blog/**' (except './src/blog/**/index.md') and
-                          updates the corresponding 'index.md'.
+                          './src/blog/**' (except './src/blog/**/index.md'),
+                          rewrites the corresponding 'index.md' files and
+                          creates 'rss.xml' files in './src/blog/' and its
+                          subdirectories (categories).
 
 -n, --new CATEGORY TITLE  creates a new file './src/blog/<CATEGORY>/<TITLE>.md'
                           and inserts the basic yaml configuration header.
@@ -22,6 +24,10 @@ import os
 import datetime
 import yaml
 
+APP_NAME = 'blog.py'
+APP_VERSION = 'v0.2'
+APP_NAME_VERSION = f'{APP_NAME} {APP_VERSION}'
+
 BLOG_PATH = 'src/blog/'
 
 KEEP_GOING_ON_YAML_ERROR = False
@@ -29,6 +35,140 @@ KEEP_GOING_ON_YAML_ERROR = False
 LOG_DEBUG = False
 LOG_INFO = LOG_DEBUG or True
 LOG_WARNING = LOG_INFO or True
+
+
+def datetime2rfc2822(dt: datetime.datetime | None = None) -> str:
+    """ returns the date and time represented by dt
+    in RFC2822 compatible format using CET (central european time)
+    with DLT (daylight saving time).
+    rss feeds use this format.
+    if dt is None it will assume now().
+    see: https://www.rfc-editor.org/rfc/rfc2822#section-3.3
+    """
+    if dt is None:
+        dt = datetime.datetime.now()
+
+    dst_start = datetime.datetime(
+        dt.year,
+        3,  # Month (March)
+        (31 - datetime.date(dt.year, 3, 31).weekday()), # Last Sunday of March
+        3,  # Hour
+        0,  # Minute
+        0,  # Second
+        1   # Microsecond
+    )
+    # same as dst_start, but ists last Sunday of October
+    dst_end = datetime.datetime(
+        dt.year, 10, (31 - datetime.date(dt.year, 10, 31).weekday()),
+        3, 0, 0, 1)
+
+    # if dst_start <= dt < dst_end:
+    #     offset = timedelta(hours=2)  # CEST (UTC+2)
+    # else:
+    #     offset = timedelta(hours=1)  # CET (UTC+1)
+    # # Convert to GMT
+    # dt_gmt = dt - offset
+    zone = '0200' if dst_start < dt < dst_end else '0100'
+
+    return dt.strftime(f'%a, %d %b %Y %H:%M:%S {zone}')
+
+
+def date2rfc2822(d: datetime.date | None = None) -> str:
+    """ same as 'datetime2rfc2822()' but uses date instead of datetime,
+    which implies static 00:00:00 (midnight) for the time part.
+    """
+    if d is None:
+        d = datetime.date.today()
+    d = datetime.datetime.combine(d, datetime.time.min)
+    return datetime2rfc2822(d)
+
+
+BLOG_MAIN_INDEX_HEAD = f"""---
+title: Blog
+lang: de
+keywords:
+- blog
+description: Index of Blog
+blog-title:
+blog-date:
+nav-blog: true
+nav-blog-home: true
+blog-changelog:
+comment: created by blog.py {datetime.datetime.now():%Y-%m-%d %H:%M:%S}
+---
+<!-- created by blog.py {datetime.datetime.now():%Y-%m-%d %H:%M:%S} -->
+
+Blog
+====
+"""
+
+BLOG_CATEGORY_INDEX_HEAD = f"""---
+title: Blog({{category}})
+lang: de
+keywords:
+- blog
+- {{category}}
+description: Index of Blog about {{category}}
+blog-title:
+blog-date:
+nav-blog: true
+nav-blog-{{category}}: true
+blog-changelog:
+comment: created by blog.py {datetime.datetime.now():%Y-%m-%d %H:%M:%S}
+---
+<!-- created by blog.py {datetime.datetime.now():%Y-%m-%d %H:%M:%S} -->
+
+{{category}}
+==========
+
+<ul>
+"""
+
+BLOG_INDEX_ENTRY = (
+    '<li>'
+    '<a href="{href}">{title}</a>'
+    '<br>'
+    '<small>[{lang}] {date} ({keywords})</small>'
+    '</li>\n')
+
+BLOG_NEW_POST = f"""---
+title: {{title}}
+lang: de
+keywords:
+- blog
+- {{category}}
+- {{title}}
+description: {{category}} - {{title}}
+blog-title: {{title}}
+blog-date: {datetime.date.today():%Y-%m-%d}
+nav-blog: true
+nav-blog-{{category_dirname}}: true
+blog-changelog:
+---
+
+Hier den Blogeintrag schreiben ...
+Und nicht vergessen, den YAML Header anzupassen!
+"""
+
+RSS_TEMPLATE = f"""<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+<channel>
+  <title>{{title}}</title>
+  <link>{{link}}</link>
+  <description>{{description}}</description>
+  <lastBuildDate>{datetime2rfc2822()}</lastBuildDate>
+  <lastBuildDate>{date2rfc2822()}</lastBuildDate>
+  <generator>{APP_NAME_VERSION}</generator>
+{{items}}
+</channel>
+</rss>
+"""
+
+RSS_ITEM_TEMPLATE = """  <item>
+    <title>{title}</title>
+    <link>{link}</link>
+    <description>{description}</description>
+  </item>"""
 
 
 def message_builder(message: str, *args, **kwargs) -> str:
@@ -70,19 +210,106 @@ def ensure_project_root():
 
 
 def filename_compatible(name: str) -> str:
+    """ converts a name (f.e. blog title) in a filename friendly name """
+    # my filenames should only consist of [a-z\-0-9]
     return ''.join(c for c in (name
                                .lower()
-                               .replace(' ', '_')
+                               .replace(' ', '-')
                                .replace('/', '-')
                                .replace('\\', '-')
+                               .replace('_', '-')
                                .replace('ä', 'ae')
                                .replace('ö', 'oe')
                                .replace('ü', 'ue')
                                .replace('ß', 'sz'))
-                   if c in 'abcdefghijklmnopqrstuvwxyz_-1234567890')
+                   if c in 'abcdefghijklmnopqrstuvwxyz-1234567890')
 
 
-def update_index_files():
+def update_index_files(category_headers: dict[str, list[dict]]):
+    #warning('EARLY RETURN FOR DEBUG PURPOSES')
+    #return
+    toplevel_index_file = os.path.join(BLOG_PATH, 'index.md')
+    with open(toplevel_index_file, 'w') as toplevel_index:
+        toplevel_index.write(BLOG_MAIN_INDEX_HEAD)
+        for category, headers in category_headers.items():
+            toplevel_index.write(f"\n{category}\n---\n\n<ul>\n")
+            category_index_file = os.path.join(BLOG_PATH, category, 'index.md')
+            with open(category_index_file, 'w') as category_index:
+                category_index.write(
+                    BLOG_CATEGORY_INDEX_HEAD.format(category=category))
+                for header in headers:
+                    href = header.get('href', None)
+                    if href is None:
+                        error(0, "'href' missing in data!", **header)
+                        continue
+                    entry = {
+                        'href': href,
+                        'title': header.get('title', 'No Title Found!'),
+                        'lang': header.get('lang', 'de'),
+                        'keywords': '|'.join(
+                            header.get('keywords', ['blog', category])),
+                        'date': header.get('blog-date', 'unknown')}
+                    category_index.write(BLOG_INDEX_ENTRY.format(**entry))
+                    toplevel_index.write(BLOG_INDEX_ENTRY.format(**entry))
+                category_index.write("</ul>")
+        toplevel_index.write("</ul>\n\n")
+
+
+def analyze_nested_types(x, prefix=''):
+    print(f'{prefix}{type(x)}{getattr(x, "__name__", "")}')
+    if isinstance(x, dict):
+        for k, v in x.items():
+            print(f'{prefix}>Key: ', end='')
+            analyze_nested_types(k, f'  {prefix}')
+            print(f'{prefix}>Value: ', end='')
+            analyze_nested_types(v, f'  {prefix}')
+            print(prefix, end='')
+            print('-' * (len(prefix) + 10))
+    elif isinstance(x, (list, set, tuple)):
+        for i in x:
+            analyze_nested_types(i, f'  {prefix}')
+            print(prefix, end='')
+            print('-' * (len(prefix) + 10))
+
+
+def update_rss_files(category_headers: dict[str, list[dict]]):
+    all_items = []
+    category_items = {}
+    for category, headers in category_headers.items():
+        category_items[category] = []
+        for header in headers:
+            if 'href' not in header:
+                continue
+            item = RSS_ITEM_TEMPLATE.format(
+                title=header.get('title', 'unknown'),
+                link=f"https://lukas-singer.eu{header.get('href')}",
+                description=header.get('description', ''))
+            all_items.append(item)
+            category_items[category].append(item)
+
+    toplevel_feed_file = os.path.join(BLOG_PATH, 'rss.xml')
+    with open(toplevel_feed_file, 'w') as toplevel_feed:
+        toplevel_feed.write(
+            RSS_TEMPLATE.format(
+                title="(.*) auf lukas-singer.eu",
+                link="https://lukas-singer.eu/blog",
+                description="Der gesamte Blog auf lukas-singer.eu",
+                items='\n'.join(all_items)))
+
+    for category in category_items:
+        category_feed_file = os.path.join(BLOG_PATH, category, 'rss.xml')
+        with open(category_feed_file, 'w') as category_feed:
+            category_feed.write(
+                RSS_TEMPLATE.format(
+                    title=f"{category} auf lukas-singer.eu",
+                    link=f"https://lukas-singer.eu/blog/{category}",
+                    description=f"Der Blog über {category} auf lukas-singer.eu",
+                    # TODO !!!
+                    items='\n'.join(all_items)))
+
+
+def update_blog():
+    # read categories and yaml headers
     categories: list[str] = [
         dir
         for dir in os.listdir(BLOG_PATH)
@@ -94,7 +321,9 @@ def update_index_files():
         entries: list[str] = [
             e
             for e in os.listdir(cat_dir)
-            if os.path.isfile(os.path.join(cat_dir, e)) and e != 'index.md']
+            if (os.path.isfile(os.path.join(cat_dir, e))
+                and e != 'index.md'
+                and e.endswith('.md'))]
         for entry in entries:
             entry_file = os.path.join(cat_dir, entry)
             debug(f"{category=}, {cat_dir=}, {entry=}, {entry_file=}")
@@ -124,78 +353,10 @@ def update_index_files():
                 error(None if KEEP_GOING_ON_YAML_ERROR else 1,
                       f'Failed to parse YAML Header in "{entry_file}" '
                       f'{"Continue" if KEEP_GOING_ON_YAML_ERROR else "Stop"}!')
-    # TODO: index.md files befüllen.
     debug(category_headers)
-    toplevel_index_file = os.path.join(BLOG_PATH, 'index.md')
-    with open(toplevel_index_file, 'w') as toplevel_index:
-        toplevel_index.write(f"""---
-title: Blog
-lang: de
-keywords:
-- blog
-- lukas singer
-description: Index of Blog
-blog-title:
-blog-date:
-nav-blog: true
-blog-changelog:
-comment: created by blog.py {datetime.datetime.now():%Y-%m-%d %H:%M:%S}
----
-<!-- created by blog.py {datetime.datetime.now():%Y-%m-%d %H:%M:%S} -->
-
-Blog
-====
-""")
-        for category, headers in category_headers.items():
-            toplevel_index.write(f"""
-{category}
-----------
-
-<ul>
-""")
-            category_index_file = os.path.join(BLOG_PATH, category, 'index.md')
-            with open(category_index_file, 'w') as category_index:
-                category_index.write(f"""---
-title: Blog({category})
-lang: de
-keywords:
-- blog
-- lukas singer
-- {category}
-description: Index of Blog about {category}
-blog-title:
-blog-date:
-nav-blog: true
-nav-blog-{category}: true
-blog-changelog:
-comment: created by blog.py {datetime.datetime.now():%Y-%m-%d %H:%M:%S}
----
-<!-- created by blog.py {datetime.datetime.now():%Y-%m-%d %H:%M:%S} -->
-
-{category}
-==========
-
-<ul>
-""")
-                for header in headers:
-                    href = header.get('href', None)
-                    if href is None:
-                        error(0, "'href' missing in data!", **header)
-                        continue
-                    title = header.get('title', 'No Title Found!')
-                    lang = header.get('lang', 'de')  # default website language
-                    keywords = header.get('keywords', ['blog', category])
-                    date = header.get('blog-date', None)
-                    category_index.write(f"""<li>
-<a href="{href}">{title}</a><br><small>[{lang}] {date} ({'|'.join(keywords)})</small>
-</li>
-""")
-                    toplevel_index.write(f"""<li>
-<a href="{href}">{title}</a><br><small>[{lang}] {date} ({'|'.join(keywords)})</small>
-</li>
-""")
-                category_index.write("</ul>")
-        toplevel_index.write("</ul>\n\n")
+    # pass categories and headers to index-builder and rss-builder
+    update_index_files(category_headers)
+    update_rss_files(category_headers)
 
 
 def new_blog_entry(category: str, title: str) -> None:
@@ -204,36 +365,32 @@ def new_blog_entry(category: str, title: str) -> None:
         for dir in os.listdir(BLOG_PATH)
         if os.path.isdir(os.path.join(BLOG_PATH, dir))]
     category_dirname = filename_compatible(category)
+    cat_dir = os.path.join(BLOG_PATH, category_dirname)
     if category_dirname not in categories:
         warning(f'New Category: {category_dirname} ({category})!')
-    cat_dir = os.path.join(BLOG_PATH, category_dirname)
+        print('You are about to create a new blog category.')
+        print(f'This will create the directory "{cat_dir}"')
+        print('Make sure to add it to template.html')
+        print('grep for "blog-nav-*"')
+        answer = ''
+        while answer not in ('y', 'yes', 'n', 'no'):
+            answer = input('Create new category? (y/n)').lower()
+        if answer not in ('y', 'yes'):
+            error(0, 'User canceled.')
+            return  # unreachable error() calls exit()
     os.makedirs(cat_dir, exist_ok=True)
 
     entry_file = os.path.join(cat_dir, f"{filename_compatible(title)}.md")
     if os.path.exists(entry_file):
         error(1, f'Entry "{entry_file}" already exists! Skipping...')
-        return
+        return  # unreachable error() calls exit()
 
     with open(entry_file, 'w') as file:
-        file.write(f"""---
-title: {title}
-lang: de
-keywords:
-- blog
-- lukas singer
-- {category}
-- {title}
-description: {category} - {title}
-blog-title: {title}
-blog-date: {datetime.date.today():%Y-%m-%d}
-nav-blog: true
-nav-blog-{category_dirname}: true
-blog-changelog:
----
-
-Hier den Blogeintrag schreiben ...
-Und nicht vergessen, den YAML Header anzupassen!
-""")
+        file.write(
+            BLOG_NEW_POST.format(
+                title=title,
+                category=category,
+                category_dirname=category_dirname))
     info(f'New blog entry created: "{entry_file}"')
 
 
@@ -245,7 +402,7 @@ if __name__ == '__main__':
         print(__doc__.strip())
     elif len(sys.argv) == 2 and sys.argv[1] in ('-u', '--update'):
         ensure_project_root()
-        update_index_files()
+        update_blog()
     elif len(sys.argv) == 4 and sys.argv[1] in ('-n', '--new'):
         category = sys.argv[2]
         title = sys.argv[3]
